ee05fd9f04339ee457842e4d28d4e24833ca40dc
lewisgoing <lgoing7@uw.edu>
2025-02-24 22:10:28 -0800

    Added audio player

---
+++ animated-music-player/app/page.tsx
@@ -0,0 +1,10 @@
+import { MusicPlayer } from "@/components/music-player"
+
+export default function Page() {
+  return (
+    <div className="min-h-screen p-4 flex justify-center items-start pt-16 bg-neutral-50">
+      <MusicPlayer />
+    </div>
+  )
+}
+

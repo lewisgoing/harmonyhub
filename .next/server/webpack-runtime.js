e2da23cbcbd2f27121d2830cb1cc0f6400488ff0
lewisgoing <lgoing7@uw.edu>
2025-03-03 13:59:16 -0800

    fixed dependancies

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("a4e970986b9f5312")
+/******/ 		__webpack_require__.h = () => ("58e00e4eba83fca9")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ba765a7b05855535"; }
+/******/ 		__webpack_require__.h = function() { return "a5b74c336772557a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
